b1713d4b4b99a72f04a4728d3133a3286bd967b0
dev <dev@devs.local>
2021-12-09 23:44:23 +0300

    creates stored procedure

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -2,14 +2,14 @@
 DELIMITER |
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-  UPDATE users AS U,
-    (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg
+UPDATE users AS U, (
+    SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg
     FROM users AS U
     JOIN corrections as C ON U.id=C.user_id
     JOIN projects AS P ON C.project_id=P.id
-    GROUP BY U.id)
-  AS WA
-  SET U.average_score = WA.w_avg
-  WHERE U.id=WA.id;
+    GROUP BY U.id
+) AS WA
+SET U.average_score = WA.w_avg
+WHERE U.id=WA.id;
 END;
 |
